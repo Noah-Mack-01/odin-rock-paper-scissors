30bf51205925238d13f449a2a95cea6017fe5137
Noah-Mack-01 <noahnewmanmack@gmail.com>
2022-11-15 08:07:51 -0500

    establish gui; add styling; rework html.
    Reworked the javascript to respond to button events on the webpage.
    Added a console log to the html alongside buttons and scorekeeping text
    in order to make sure events were being recorded by the scripts.
    TODO: Add a final message to the game after rounds are eliminated.

--- rps.js
+++ rps.js
@@ -19,10 +19,55 @@ res.set("paper", "scissors");
 res.set("scissors", "rock");
 res.set("rock","paper");
 
+let numRounds = 5;
+let curChoice ="rock", score = 0, opp = 0, rounds = numRounds;
+document.getElementById("rock-button").classList.add("selected");
 
-let decideWinner = (player, computer) => {
+let refresh = () => {
+    document.getElementById("pscr").innerHTML = score;
+    document.getElementById("ct").innerHTML = rounds;
+    document.getElementById("cscr").innerHTML = opp;
 
-    let player1 = player.toLowerCase();
+    if (rounds <= 0 || opp > numRounds/2 || score > numRounds / 2) {
+        document.getElementById("play-button").classList.add("game-over");
+        document.getElementById("rock-button").classList.add("game-over");
+        document.getElementById("paper-button").classList.add("game-over");
+        document.getElementById("scissors-button").classList.add("game-over");
+    }
+
+}
+
+let update = (log) => document.getElementById("log").innerHTML = log;
+
+document.getElementById("rock-button").onclick = () => { 
+    document.getElementById(`${curChoice}-button`).classList.remove("selected"); 
+    curChoice="rock"; document.getElementById("rock-button").classList.add("selected");
+    update("Selected Rock!")
+}
+
+
+document.getElementById("paper-button").onclick = () => { 
+    document.getElementById(`${curChoice}-button`).classList.remove("selected");
+    curChoice="paper"; document.getElementById("paper-button").classList.add("selected");
+    update("Selected Paper!");
+}
+
+
+document.getElementById("scissors-button").onclick = () => {
+    document.getElementById(`${curChoice}-button`).classList.remove("selected");
+    curChoice="scissors"; document.getElementById("scissors-button").classList.add("selected");
+    update("Selected Scissors!"); 
+}
+
+document.getElementById("play-button").onclick = () => {
+    if (rounds == 0 || opp > numRounds / 2 || score > numRounds / 2) return;
+    game();
+    refresh();
+}
+
+let decideWinner = (computer) => {
+
+    let player1 = curChoice;
     let computer1 = computer.toLowerCase();
 
     if (player1 == computer1) return 0;
@@ -31,9 +76,35 @@ let decideWinner = (player, computer) => {
 }
 
 let game = () => {
+    let res = decideWinner(getComputerChoice());
+    switch (res) {
+        case 1:
+            ++score;
+            --rounds;
+            update("You won the round.");
+            break;
+        case 0:
+            --rounds;
+            update("Tie round!");
+            break;
+        default:
+            update("Computer wins the round.");
+            --rounds;
+            ++opp;
+            break;
+    }
+
+
+}
+
+/**
+let game = () => {
     let score = 0, opp=0;
     for (i=0; i<5; i++){
-        let ans = prompt("Rock, Paper or Scissors?");
+
+        if (opp > 2 || score > 2) break;
+
+        let ans = curChoice;
         console.log(ans.toLowerCase());
         if (res.get(ans.toLowerCase()) == undefined) {
             console.log("Illegal Input! Try Again");
@@ -45,11 +116,14 @@ let game = () => {
         switch(singleGame){
             case 1:
                 score++;
+                document.querySelector("#player-score.span").innerHTML = `${score}`;
+                document.querySelector("#counter.span").innerHTML = `${5-i}`
                 break;
             case 0:
                 break;
             default:
                 opp++;
+                document.querySelector("#comp-score.span").innerHTML = `${opp}`;
                 break;
         }
     }
@@ -68,4 +142,8 @@ let game = () => {
             else console.log("Computer wins!");
             break;
     }
-}
+}*/
+
+
+refresh();
+console.log(document.getElementById("scissors-button").innerHTML);
